09740a497e2b3ebdd44895a58fb393b8aa2351d9
Drew Terrell <drewterrell29@gmail.com>
2025-03-06 16:33:00 +0000

    chapter 3

---
+++ chapter-03/3-8-seeing-the-world.py
@@ -0,0 +1,19 @@
+places = ["Colorado", "California", "New Zealand", "Iceland", "Arkansas"]
+print(places)
+print(sorted(places))
+print(places)
+
+print(sorted(places, reverse=True))
+print(places)
+
+places.reverse()
+print(places)
+
+places.reverse()
+print(places)
+
+places.sort()
+print(places)
+
+places.sort(reverse=True)
+print(places)
